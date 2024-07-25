96a50002b8cd7701bbbc50d9f8a3ad9bcfcfca69
daniel cooder <danielcooder@gmail.com>
2024-07-25 20:18:23 -0300

    feat: vetor

---
+++ src/main/java/org/example/Vetor.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Vetor {
+
+
+    public static void main(String[] args) {
+
+      int n[] = {3,2,8,7,5,4,2};
+
+      for (int c=0; c<=n.length-1; c++ ){
+          System.out.println("Na posição " + c + " temos o valor " + n[c] + "" );
+      }
+
+    }
+}
